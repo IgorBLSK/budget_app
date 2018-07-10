b7dbe73f04f2c10ab3db206f73320be601e92501
Igor <igor.koska@gmail.com>
2018-07-10 19:18:33 +0200

    data structure added

--- app.js
+++ app.js
@@ -1,44 +1,107 @@
 // BUGDET CONTROLLER
-var budgetController = (function() {
+var budgetController = (function () {
 
-    
-})();
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
-// UI CONTROLLER
-var UIController = (function() {
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
 
-    var DOMStrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value'
     };
 
     return {
-        getInput: function() {
-            return {
-                type: document.querySelector(DOMStrings.inputType).value,
-                description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
-            };
-           
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            // create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // create new item based on type
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val);
+            } else if (type === "inc") {
+                newItem = new Income(ID, des, val);
+            }
+
+            // push to data structure and return
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
         }
     };
 
-
 })();
 
+// UI CONTROLLER
+var UIController = (function () {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings
+        },
+    };
+})();
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function() {
-          
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (e) {
+            if (e.keyCode === 13 || e.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
+        var input, newItem;
+
         // 1. get field imput data
-        var input = UICtrl.getInput();
-        console.log(input);
+        input = UICtrl.getInput();
 
         // add item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // add item to UI
 
@@ -46,17 +109,15 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // display total budget
 
-        console.log("TEST ctrlAddItem")
-    }
+    };
 
-    // Add button
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    // "Enter" keypress
-    document.addEventListener('keypress', function(e) {
-        if(e.keyCode === 13 || e.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function () {
+            console.log("App started");
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
